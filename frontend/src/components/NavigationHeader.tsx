cbb9434ecb3fdddfff96294bc18c1f60effb875f
leewooseong <sitpo96@naver.com>
2024-01-22 08:59:57 +0900

    [FE] Feat #8: 레이아웃 애니메이션 적용
    - onClick 방식으로 underline을 구현하려고 했으나 초기값이 url과 맞지 않는 값으로 설정됨
    - 따라서 구현 방법을 currentPath를 이용한 방법으로 구현
    
    Fixes: #8

--- frontend/src/components/NavigationHeader.tsx
+++ frontend/src/components/NavigationHeader.tsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { MultiPageHeaderInfo } from '../types/router'
+import { motion } from 'framer-motion'
+import { useRouter } from '../hooks/useRouter'
 
 interface NavigationHeaderProps {
 	headerData: MultiPageHeaderInfo[]
 }
 
 const NavigationHeader = ({ headerData }: NavigationHeaderProps) => {
+	const [selectedIndex, setSelectedIndex] = useState<number>()
+	const { currentPath } = useRouter()
+	useEffect(() => {
+		headerData.forEach((item, index) => {
+			if (currentPath.includes(item.path)) {
+				setSelectedIndex(index)
+			}
+		})
+	})
+
 	return (
-		<ul>
+		<ul className={`flex gap-[2px]`}>
 			{headerData.map((headerItem, index) => {
 				return (
-					<li key={`hedaer-${index}`}>
+					<li key={`hedaer-${index}`} className={`w-16 relative`}>
 						<NavLink
 							to={headerItem.path}
+							data-id={index}
 							className={({ isActive }) =>
-								isActive ? 'text-[#454645] font-[700]' : 'text-[#D9D9D9]'
+								'inline-block w-full text-center py-2' +
+								(isActive ? ' text-[#454645] font-[700]' : ' text-[#D9D9D9]   ')
 							}
 						>
 							{headerItem.name}
 						</NavLink>
+						{selectedIndex === index && (
+							<motion.div
+								layoutId="underline"
+								className="absolute h-[1px] bottom-1 w-full bg-black"
+							/>
+						)}
 					</li>
 				)
 			})}
