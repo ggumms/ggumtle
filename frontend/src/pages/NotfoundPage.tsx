ecd89231b3d7a9f6f587d8e3059f8a3b964ce350
이우성 <sitpo96@naver.com>
2024-01-22 14:20:34 +0900

    [FE] Setting #34: react-icon 추가
    Fixes: #34

--- frontend/src/pages/NotfoundPage.tsx
+++ frontend/src/pages/NotfoundPage.tsx
@@ -1,5 +1,6 @@
 // NotFoundPage.tsx
 import React from 'react'
+import { FaBan } from 'react-icons/fa'
 import { useRouter } from '../hooks/useRouter'
 
 const NotFoundPage: React.FC = () => {
@@ -11,12 +12,15 @@ const NotFoundPage: React.FC = () => {
 	return (
 		<div className="flex items-center justify-center h-screen bg-gray-100">
 			<div className="text-center">
-				<h1 className="text-6xl font-bold text-gray-800">404</h1>
+				<h1 className="flex items-center justify-center text-6xl font-bold text-gray-800 align-middle">
+					<FaBan size={40} />
+					<p>404</p>
+				</h1>
 				<p className="text-2xl font-light text-gray-600">Page not found</p>
 				<p className="mt-4 text-gray-500">The page you are looking for does not exist.</p>
 				<button
 					onClick={handleClickGoHomeBtn}
-					className="p-1 px-4 py-2 mt-6 font-bold text-white bg-blue-500 border-b-2 hover:bg-blue-600"
+					className="p-1 px-4 py-2 mt-6 font-bold text-white border-b-2 hover:text-blue-600"
 				>
 					Go Home
 				</button>
