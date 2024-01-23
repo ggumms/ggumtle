442c52ba9970f1542bd5360f30dd3b03a7106e6d
이원주 <dldnjswn4265@gmail.com>
2024-01-23 22:44:27 +0900

    [FE] Feat #20: 유저 아이콘 컴포넌트 제작
    - ProfileBucket.tsx 버킷 컴포넌트 수정된 부분
      - tailwind는 동적으로 생성된 클래스는 인식하지 않는 문제를 발견
      - 객체를 만들어적용하는 방식으로 변경함
    
    Change-Id: I49fbf1194aaada6e302c0c1c07639cab9c6c126b

---
+++ frontend/src/pages/Radar/components/FollowingTab.tsx
@@ -0,0 +1,14 @@
+import UserItem from './UserItem'
+
+const FollowingTab = () => {
+	return (
+		<div>
+			This is Following Tab
+			<UserItem />
+			<UserItem />
+			<UserItem />
+		</div>
+	)
+}
+
+export default FollowingTab
