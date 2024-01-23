58da206673be4f2400594fe67837e643afb7835c
이원주 <dldnjswn4265@gmail.com>
2024-01-24 00:38:27 +0900

    [FE] Feat #22: 레이더 망 UI 및 애니메이션 구현
    Change-Id: I69ae7f7a10cffadbd67ca499b6212630c52b120e

--- frontend/src/pages/Radar/Radar.tsx
+++ frontend/src/pages/Radar/Radar.tsx
@@ -1,36 +1,15 @@
-import { Outlet, useNavigate } from 'react-router-dom'
-import Header from '../../components/Header'
-import { IMenu, IMenuFunc } from '../../interfaces'
-import { icons } from '../../constants/header-icons'
-import ProfileBucket from '../../components/ProfileBucket'
-
+import { Outlet } from 'react-router-dom'
 const Radar = () => {
-	// 헤더 컴포넌트 사용 예시입니다.
-	const navigate = useNavigate()
-
-	// 각각 왼쪽 오른쪽 중앙 제목에 나타낼 아이콘, 제목 지정
-	const menu: IMenu = {
-		left: icons.BACK, // 왼쪽에 나타낼 아이콘 (./src/constants/header-icons 에 지정해두고 사용)
-		center: '방명록', // 헤더 컴포넌트에 나타낼 헤더 제목
-		right: undefined, // 사용하지 않는 쪽은 undefined 지정
-	}
-
-	// 왼쪽 오른쪽 아이콘에 할당할 함수 설정
-	const func: IMenuFunc = {
-		left_func: () => navigate(-1),
-		right_func: undefined, // 마찬가지로 사용하지 않는 쪽은 undefined 지정
-	}
-	const bucketInfo = {
-		title: '구독자 100만명 달성하기',
-		color: 'green',
-		dDay: 168,
-		isLock: true,
-	}
+	// const bucketInfo = {
+	// 	title: '구독자 100만명 달성하기',
+	// 	color: 'green',
+	// 	dDay: 168,
+	// 	isLock: true,
+	// }
 
 	return (
 		<div>
-			<Header menu={menu} func={func} />
-			<ProfileBucket bucketInfo={bucketInfo} />
+			{/* <ProfileBucket bucketInfo={bucketInfo} /> */}
 			<Outlet />
 		</div>
 	)
--- frontend/src/pages/Radar/components/FollowingTab.tsx
+++ frontend/src/pages/Radar/components/FollowingTab.tsx
@@ -1,16 +1,11 @@
-import BucketItem from './BucketItem'
-import UserItem from './UserItem'
-
 const FollowingTab = () => {
 	return (
-		<div>
-			This is Following Tab
-			<UserItem />
-			<UserItem />
-			<UserItem />
-			<BucketItem />
-			<BucketItem />
-			<BucketItem />
+		<div
+			className="w-full animate-radar1 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center"
+		>
+			<div className="w-2/3 animate-radar2 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center">
+				<div className="w-1/2 animate-radar3 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center"></div>
+			</div>
 		</div>
 	)
 }
