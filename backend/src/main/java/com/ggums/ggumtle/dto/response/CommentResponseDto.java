d86eacb1c38a7934c5014657f7814e911a1f6313
전지환 <jeonji0109@naver.com>
2024-01-24 17:23:15 +0900

    [BE] Feat #13: 댓글 CRUD 기능 추가
    - 단일 댓글 데이터 조회 기능 누락 : 추가 예정
    - 댓글 목록 반환 기능에 ( 조회 시간 - 작성 시간 ) 로직 필요
    - 페이지네이션 적용 완료
    
    Change-Id: Ib468c877bf6a6bfb4f8ea54a72e62815777d8358

---
+++ backend/src/main/java/com/ggums/ggumtle/dto/response/CommentResponseDto.java
@@ -0,0 +1,24 @@
+package com.ggums.ggumtle.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+public class CommentResponseDto {
+
+    private long id;
+    private long userId;
+    private String userNickname;
+    private String context;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime updatedDate;
+    private long numberOfLikes;
+}
---
+++ backend/src/main/java/com/ggums/ggumtle/service/CommentBucketService.java
@@ -0,0 +1,101 @@
+package com.ggums.ggumtle.service;
+
+
+import com.ggums.ggumtle.common.exception.CustomException;
+import com.ggums.ggumtle.common.exception.ExceptionType;
+import com.ggums.ggumtle.dto.request.CommentRequestDto;
+import com.ggums.ggumtle.dto.response.CommentResponseDto;
+import com.ggums.ggumtle.dto.response.model.BucketSearchListDto;
+import com.ggums.ggumtle.entity.Bucket;
+import com.ggums.ggumtle.entity.CommentBucket;
+import com.ggums.ggumtle.entity.User;
+import com.ggums.ggumtle.repository.BucketRepository;
+import com.ggums.ggumtle.repository.CommentBucketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentBucketService {
+
+    private final BucketRepository bucketRepository;
+    private final CommentBucketRepository commentBucketRepository;
+
+    public String commentCreate(User user, long bucketId, CommentRequestDto requestDto) {
+
+        Bucket bucket = bucketRepository.findById(bucketId)
+                .orElseThrow(()->new CustomException(ExceptionType.BUCKET_NOT_FOUND));
+
+        CommentBucket commentBucket = CommentBucket.builder()
+                .user(user)
+                .bucket(bucket)
+                .context(requestDto.getContext())
+                .build();
+
+        commentBucketRepository.save(commentBucket);
+        return "댓글이 생성되었습니다.";
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CommentResponseDto> commentList(User user, long bucketId, Pageable pageable) {
+
+        Bucket bucket = bucketRepository.findById(bucketId)
+                .orElseThrow(()->new CustomException(ExceptionType.BUCKET_NOT_FOUND));
+
+        // 비공개일 때, 요청한 사용자와 버켓 글쓴이가 다르면 오류 반환
+        if (bucket.getIsPrivate() && !(bucket.getUser().getId().equals(user.getId()))) {
+            throw new CustomException(ExceptionType.NOT_VALID_USER);
+        }
+
+        Page<CommentBucket> comments = commentBucketRepository.findByBucket(bucket, pageable);
+        return comments.map(this::convertToCommentResponseDto);
+    }
+
+    private CommentResponseDto convertToCommentResponseDto(CommentBucket item) {
+        return CommentResponseDto.builder()
+                .id(item.getId())
+                .userId(item.getUser().getId())
+                .userNickname(item.getUser().getUserNickname())
+                .context(item.getContext())
+                .createdDate(item.getCreatedDate())
+                .updatedDate(item.getUpdatedDate())
+                .numberOfLikes(item.getCommentBucketLikes().size())
+                .build();
+    }
+
+    public String CommentDelete(User user, long commentId) {
+
+        CommentBucket comment = commentBucketRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ExceptionType.COMMENT_NOT_FOUND));
+
+        // 요청한 사용자와 댓글 작성자가 다르면 오류 반환
+        if (!(comment.getUser().getId().equals(user.getId()))) {
+            throw new CustomException(ExceptionType.NOT_VALID_USER);
+        }
+
+        commentBucketRepository.delete(comment);
+        return "댓글이 삭제되었습니다.";
+    }
+
+    public String CommentUpdate(User user, CommentRequestDto requestDto, long commentId) {
+
+        CommentBucket comment = commentBucketRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ExceptionType.COMMENT_NOT_FOUND));
+
+        // 요청한 사용자와 댓글 작성자가 다르면 오류 반환
+        if (!(comment.getUser().getId().equals(user.getId()))) {
+            throw new CustomException(ExceptionType.NOT_VALID_USER);
+        }
+
+        comment.update(requestDto.getContext());
+        commentBucketRepository.save(comment);
+        return "댓글이 수정되었습니다.";
+    }
+}
+
