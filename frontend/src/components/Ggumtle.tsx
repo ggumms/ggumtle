c2b82311606b479ae168d244a3cf428f3db731a8
이우성 <sitpo96@naver.com>
2024-01-25 11:18:48 +0900

    [FE] Feat #10: 꿈틀 컴포넌트 개발
    - color, height, width, speed, text를 입력받을 수 있도록 컴포넌트 개발
    - 아무의미 없는 div tag에 설명을 붙이기 위해서 explanation props 추가
    - 레이더 화면의 검색과 같이 icon을 넣어야하는 경우를 위해서 children 추가
    
    Fixes: #10
    
    Change-Id: Id6c3219a8dac12acecc437ec7d05c3077b406c2c

--- frontend/src/components/Ggumtle.tsx
+++ frontend/src/components/Ggumtle.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import './Ggumtle.css'
 import { bgColorClass } from '../constants/dynamicClass'
 
-const Ggumtle = () => {
+const DEFAULT_COLOR = 'green'
+const DEFAULT_WIDTH = '4vh'
+const DEFAULT_HEIGHT = '4vh'
+const DEFAULT_SPEED = 10
+
+// screen-reader 설명을 위한 explanation props 추가
+// 검색 아이콘을 위한 children props 추가
+interface IGgumtleProps {
+	color: string
+	width: string
+	height: string
+	speed: number
+	explanation?: string
+	children?: ReactNode
+}
+
+const Ggumtle = ({
+	color = DEFAULT_COLOR,
+	width = DEFAULT_WIDTH,
+	height = DEFAULT_HEIGHT,
+	speed = DEFAULT_SPEED,
+	explanation,
+	children,
+}: IGgumtleProps) => {
+	const getGgumtleSize = (width: string, height: string) => {
+		return { width, height }
+	}
+	const getGgumtleAnimation = (speed: number) => {
+		return { animation: `transform ${speed}s ease-in-out infinite both alternate` }
+	}
+	const ggumtleInlineStyle = Object.assign(
+		getGgumtleSize(width, height),
+		getGgumtleAnimation(speed)
+	)
+
 	return (
-		<>
-			<div
-				className={`ggumtle min-h-[200px] min-w-[200px] w-[4vh] h-[4vh] ${bgColorClass['green']}`}
-			/>
-		</>
+		<div style={ggumtleInlineStyle} className={`ggumtle w-[4vh] h-[4vh] ${bgColorClass[color]}`}>
+			{explanation && <p className="sr-only">{explanation}</p>}
+			{children}
+		</div>
 	)
 }
 
--- frontend/src/pages/Bucket/AchieveBucket.tsx
+++ frontend/src/pages/Bucket/AchieveBucket.tsx
@@ -4,7 +4,7 @@ const AchieveBucket = () => {
 	return (
 		<div>
 			<h1>This is AchieveBucket Page</h1>
-			<Ggumtle />
+			<Ggumtle width="8vh" height="8vh" speed={5} color="lightGreen" />
 		</div>
 	)
 }
