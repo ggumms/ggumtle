5cb92ebc843cbcbbeaeec46dc79cc5153ea0473a
이우성 <sitpo96@naver.com>
2024-01-27 16:33:16 +0900

    [FE] Feat #60: 버킷 작성페이지 헤더 및 설명 문구 컴포넌트 개발
    Change-Id: I94c183f3061ef723159755d1983de680c8f2b3fc

--- frontend/src/pages/Bucket/AddBucket/CategoryInfo.tsx
+++ frontend/src/pages/Bucket/AddBucket/CategoryInfo.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import CategorySelect from '../../../components/CategorySelect'
 import { categoryData } from '../../../utils/category'
 import NavigateButton from '../../../components/NavigateButton'
+import PageDescription from '../../../components/PageDescription'
 
 const CategoryInfo = () => {
 	return (
 		<div>
+			<PageDescription type={'categoryJoin'} />
 			<CategorySelect categoryData={categoryData} />
 			{/* 페이지 전환 시 state를 유지하는지 테스트 하기 위한 버튼 */}
 			<NavigateButton path="/bucket/write/main" isDisable={true}>
