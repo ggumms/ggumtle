cbcf4bd692f1cae444d3eb3a8efff7fdebd7d315
이우성 <sitpo96@naver.com>
2024-01-28 09:25:53 +0900

    [FE] Refact #63: 검색 편이를 위해 카테고리 데이터 구조 객체로 변경
    - 버킷 작성 페이지에서 선택된 카테고리에 대한 색 정보를 쉽게 가져오기 위해 데이터 구조 변경
    
    Change-Id: I7df95223556f4f558748d621003e507b3493dcc3

--- frontend/src/utils/typeFilter.ts
+++ frontend/src/utils/typeFilter.ts
@@ -1,7 +1,14 @@
 import { categoryData } from './category'
 import { CategoryType } from '../interfaces'
 
+// before categoryData changing
+// export const isCategoryType = (input: string): input is CategoryType => {
+// 	const matchingCount = categoryData.filter((category) => category.name === input).length
+// 	return matchingCount > 0
+// }
+
+// after categoryData changing
 export const isCategoryType = (input: string): input is CategoryType => {
-	const matchingCount = categoryData.filter((category) => category.name === input).length
+	const matchingCount = Object.keys(categoryData).filter((category) => category === input).length
 	return matchingCount > 0
 }
