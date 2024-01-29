a28e2a7373689d9d4085c88ceb28875ccab28b92
서준호 <twg0995@naver.com>
2024-01-29 10:05:22 +0900

    [BE] Feat #12: 테스트 관련 코드 추가
    - test 실행시 내장 h2 의존성 추가
    
    Change-Id: Ieb1bd73585a6fa3f6bed30716061a074eebe1574

--- backend/src/test/java/com/ggums/ggumtle/GgumtleApplicationTests.java
+++ backend/src/test/java/com/ggums/ggumtle/GgumtleApplicationTests.java
@@ -3,7 +3,7 @@ package com.ggums.ggumtle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GgumtleApplicationTests.class)
 class GgumtleApplicationTests {
 
 	@Test
