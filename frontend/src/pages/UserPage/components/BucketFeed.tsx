dbe59988f27514d4037985a9cd7dd550b6aaa310
이원주 <dldnjswn4265@gmail.com>
2024-01-31 01:38:25 +0900

    [FE] Style #70: 버킷 피드 공감수, 댓글수, 공유 버튼
    Change-Id: I69ac9cf28f8de7787f81c2171907372a50535952

--- frontend/src/pages/UserPage/components/BucketFeed.tsx
+++ frontend/src/pages/UserPage/components/BucketFeed.tsx
@@ -1,9 +1,12 @@
 import InterestTag from '../../../components/InterestTag'
 import ProfileBucket from '../../../components/ProfileBucket'
-import { CheerUpReaction, CoolReaction, MetooReaction } from '../../../constants/reactions'
 import { UserInfoType } from '../../../interfaces'
+import { FiShare } from 'react-icons/fi'
 import LocationInfo from './LocationInfo'
+import TotalReaction from './TotalReaction'
+import TotalComment from './TotalComment'
 
+// 하나의 피드 (버킷)
 const BucketFeed = () => {
 	const userInfo: UserInfoType = {
 		userId: 1,
@@ -22,14 +25,22 @@ const BucketFeed = () => {
 	const category: string[] = ['직장', '여행', '인간관계']
 	const photo = true
 	const location = true
+	const reactionCnt = 28
+	const commentCnt = 12
 	return (
 		<div className="bg-white px-4 py-2">
+			{/* 버킷 */}
 			<ProfileBucket title={bucketTitle} color={color} dayCount={dayCount} isLock={null} />
+
+			{/* 태그 */}
 			<div className="bg-white">
 				{category.map((cate, index) => (
 					<InterestTag tag={cate} key={index} />
 				))}
 			</div>
+
+			{/* 옵셔널한 정보들 (장소, 사진) */}
+			{location && <LocationInfo />}
 			{photo && (
 				<img
 					src="/public/dummy.PNG
@@ -38,11 +49,14 @@ const BucketFeed = () => {
 					className="w-full h-48 object-cover my-2 rounded-md"
 				/>
 			)}
-			{location && <LocationInfo />}
-			<div className="flex">
-				<CoolReaction />
-				<MetooReaction />
-				<CheerUpReaction />
+
+			{/* 피드 제일 하단 감정 개수, 댓글 개수, 공유 버튼 */}
+			<div className="flex justify-between items-center pb-1">
+				<div className="flex gap-2">
+					<TotalReaction count={reactionCnt} />
+					<TotalComment count={commentCnt} />
+				</div>
+				<FiShare size="0.9rem" color="#454645" />
 			</div>
 		</div>
 	)
--- frontend/src/pages/UserPage/components/LocationInfo.tsx
+++ frontend/src/pages/UserPage/components/LocationInfo.tsx
@@ -4,7 +4,7 @@ const LocationInfo = () => {
   const addressName = "용산 전자랜드"
   const addressDetail = "서울 용산구 청파로 74"
 	return (
-		<div className="border border-lightGray rounded-sm h-12 flex items-center px-2 gap-2">
+		<div className="border border-lightGray rounded-sm h-12 flex items-center px-2 my-2 gap-2">
 			{/* @TODO: 버킷 고유 색상 적용시키기 */}
 			<HiLocationMarker size="1.7rem" color="#AAD4D4" />
 			<div>
