fc90252f2d3b28d0b902bdd9a36804f3b5800ed7
조인화 <ihnhwa8362@naver.com>
2024-01-31 11:07:35 +0900

    [BE] Feat #67: 친밀도 증감 수치 초기화
    Change-Id: I74aeaccc5e93ca2323f1bb3d81eda1687ac3ec4d

---
+++ backend/src/main/java/com/ggums/ggumtle/common/constant/Score.java
@@ -0,0 +1,10 @@
+package com.ggums.ggumtle.common.constant;
+
+public class Score {
+
+    public static final int READ = 1;
+    public static final int REACTION = 1;
+    public static final int COMMENT = 1;
+    public static final int COMMENT_LIKE = 1;
+
+}
