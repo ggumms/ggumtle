03dcc65f32a22348ead1b4e6322270d5869d8143
서준호 <twg0995@naver.com>
2024-02-01 10:12:14 +0900

    [INFRA] Deploy #99: 프론트엔드 빌드 스크립트 수정
    Change-Id: I6d94f16f96dab9ec9dce736ecd4b826350bf9c1a

--- deploy/ggumtle-build-frontend.sh
+++ deploy/ggumtle-build-frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ../frontend && rm -rf dist && rm -rf package-lock.json && npm install && npm run build
+cd ../frontend && rm -rf dist && rm -rf node_modules && rm -rf package-lock.json && npm install && npm run build
