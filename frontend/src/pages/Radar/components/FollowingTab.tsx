50d487784f2d7edcf8d3ffc486ea7d8edf3cad3e
이원주 <dldnjswn4265@gmail.com>
2024-02-01 22:02:56 +0900

    [FE] Refact #71: bucketArea 스타일 코드 tailwind로 수정
    Change-Id: Id1eeff1d3f8096917b8100488b2487183177b839

--- frontend/src/pages/Radar/components/FollowingTab.tsx
+++ frontend/src/pages/Radar/components/FollowingTab.tsx
@@ -10,25 +10,6 @@ const users1 = ['a', 'b', 'c', 'd', 'e', 'f']
 const users2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 const users3 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'o', 'p', 'q']
 
-// 버킷들을 띄울 div 스타일 지정
-
-// @TODO: 추후 tailwind 코드로 수정하기 (translate 속성 유의)
-const bucketArea: {
-	position: 'absolute'
-	top: string
-	left: string
-	transform: string
-	width: string
-	aspectRatio: string
-} = {
-	position: 'absolute',
-	top: 'calc(50% - 20px)',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: '110%',
-	aspectRatio: '1 / 1',
-}
-
 // @TODO: 알림 페이지에서 뒤로가기 했을때 레이더 리렌더링 되지 않도록 수정하기
 const FollowingTab = () => {
 	const [buckets, setBuckets] = useState<IBucket[]>([])
@@ -77,13 +58,13 @@ const FollowingTab = () => {
 	return (
 		<div className="">
 			<div className="w-full h-[calc(100vh-5rem)] flex justify-center items-center overflow-hidden">
-				<div className="w-[110%] mt-10 animate-radar3 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center">
+				<div className="w-[110%] mb-5 animate-radar3 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center">
 					<div className="w-2/3 animate-radar2 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center">
 						<div className="w-1/2 animate-radar1 border border-[#c6c6c661] aspect-square rounded-full flex absolute items-center justify-center"></div>
 					</div>
 				</div>
 
-				<div className="mt-10" style={bucketArea}>
+				<div className="absolute top-[calc(50%-5px)] left-1/2 w-[110%] aspect-square transform translate-x-[-50%] translate-y-[-50%]">
 					{buckets.map((item, index) => (
 						// @TODO: 레이더 라인별 UserItem 크기 조절
 						<UserItem
