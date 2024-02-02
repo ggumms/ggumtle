126ff9e33aeb4f90e2c77fc6231887493cc8baf5
전지환 <jeonji0109@naver.com>
2024-02-02 13:58:18 +0900

    [BE] Refact #92: swagger편집 브랜치 redis반영 버전
    - redis 설정을 미룰 수 없어서 버전 반영 했읍니다
    - 컨플릭트가 있었어서 최신 dev 브랜치에서 컨트롤러 부분만 추가 수정했읍니다
    
    Change-Id: Ib6f8e84c0120364193ebdcc3edf02ca37701ac8e

--- backend/src/main/java/com/ggums/ggumtle/controller/AlarmController.java
+++ backend/src/main/java/com/ggums/ggumtle/controller/AlarmController.java
@@ -22,51 +22,51 @@ public class AlarmController {
     private final AlarmService alarmService;
     private final AlarmHandler alarmHandler;
 
-    @GetMapping(value = "/subscribe", produces = "text/event-stream")
-    public SseEmitter subscribe(@AuthenticationPrincipal User user) {
-        Long userId = user.getId();
-        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        try {
-            emitter.send(SseEmitter.event().name("INIT"));
-        } catch (IOException e) {
-            throw new CustomException(ExceptionType.SSE_EMITTER_ERROR);
-        }
+//    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+//    public SseEmitter subscribe(@AuthenticationPrincipal User user) {
+//        Long userId = user.getId();
+//        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+//        try {
+//            emitter.send(SseEmitter.event().name("INIT"));
+//        } catch (IOException e) {
+//            throw new CustomException(ExceptionType.SSE_EMITTER_ERROR);
+//        }
+//
+//        // if previous emitter exist, complete(delete)
+//        SseEmitter previousEmitter = alarmHandler.userEmitters.put(userId, emitter);
+//        if (previousEmitter != null) {
+//            previousEmitter.complete();
+//        }
+//
+//        emitter.onCompletion(() -> alarmHandler.userEmitters.remove(userId, emitter));
+//        emitter.onTimeout(() -> alarmHandler.userEmitters.remove(userId, emitter));
+//        emitter.onError((e) -> alarmHandler.userEmitters.remove(userId, emitter));
+//
+//        return emitter;
+//    }
 
-        // if previous emitter exist, complete(delete)
-        SseEmitter previousEmitter = alarmHandler.userEmitters.put(userId, emitter);
-        if (previousEmitter != null) {
-            previousEmitter.complete();
-        }
+//    @GetMapping("/count")
+//    public Response alarmCount(@AuthenticationPrincipal User user){
+//        return new Response("alarmCount", alarmService.alarmCount(user));
+//    }
 
-        emitter.onCompletion(() -> alarmHandler.userEmitters.remove(userId, emitter));
-        emitter.onTimeout(() -> alarmHandler.userEmitters.remove(userId, emitter));
-        emitter.onError((e) -> alarmHandler.userEmitters.remove(userId, emitter));
+//    @GetMapping("/")
+//    public Response alarm(@AuthenticationPrincipal User user, @RequestParam Pageable pageable){
+//        return new Response("alarm", alarmService.alarmList(user, pageable));
+//    }
 
-        return emitter;
-    }
+//    @PostMapping("/{alarmId}")
+//    public Response alarmRead(@AuthenticationPrincipal User user, @PathVariable("alarmId") Long alarmId){
+//        return new Response("message", alarmService.alarmRead(user, alarmId));
+//    }
 
-    @GetMapping("/count")
-    public Response alarmCount(@AuthenticationPrincipal User user){
-        return new Response("alarmCount", alarmService.alarmCount(user));
-    }
+//    @PatchMapping("/user")
+//    public Response alarmUser(@AuthenticationPrincipal User user, @RequestParam boolean alarm){
+//        return new Response("message", alarmService.alarmUser(user, alarm));
+//    }
 
-    @GetMapping("/")
-    public Response alarm(@AuthenticationPrincipal User user, @RequestParam Pageable pageable){
-        return new Response("alarm", alarmService.alarmList(user, pageable));
-    }
-
-    @PostMapping("/{alarmId}")
-    public Response alarmRead(@AuthenticationPrincipal User user, @PathVariable("alarmId") Long alarmId){
-        return new Response("message", alarmService.alarmRead(user, alarmId));
-    }
-
-    @PatchMapping("/user")
-    public Response alarmUser(@AuthenticationPrincipal User user, @RequestParam boolean alarm){
-        return new Response("message", alarmService.alarmUser(user, alarm));
-    }
-
-    @PutMapping("/all-read")
-    public Response readAllAlarm(@AuthenticationPrincipal User user){
-        return new Response("message", alarmService.readAllAlarm(user));
-    }
+//    @PutMapping("/all-read")
+//    public Response readAllAlarm(@AuthenticationPrincipal User user){
+//        return new Response("message", alarmService.readAllAlarm(user));
+//    }
 }
--- backend/src/main/java/com/ggums/ggumtle/controller/BucketController.java
+++ backend/src/main/java/com/ggums/ggumtle/controller/BucketController.java
@@ -4,6 +4,8 @@ import com.ggums.ggumtle.common.response.Response;
 import com.ggums.ggumtle.dto.request.PostBucketReactionRequestDto;
 import com.ggums.ggumtle.dto.request.PostBucketRequestDto;
 import com.ggums.ggumtle.dto.request.UpdateBucketRequestDto;
+import com.ggums.ggumtle.dto.response.BucketSearchResponseDto;
+import com.ggums.ggumtle.dto.response.GetBucketReactionResponseDto;
 import com.ggums.ggumtle.dto.response.GetBucketResponseDto;
 import com.ggums.ggumtle.dto.response.ReviewResponseDto;
 import com.ggums.ggumtle.entity.User;
@@ -22,7 +24,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "버킷", description = "버킷과 관련된 API들 입니다")
+@Tag(name = "2. 버킷", description = "버킷과 관련된 API들 입니다")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bucket")
@@ -56,10 +58,18 @@ public class BucketController {
         return new Response("bucketInfo", bucketService.getBucket(user, bucketId));
     }
 
-//    @PutMapping("/")
-//    public Response updateBucket(@AuthenticationPrincipal User user, @Valid @RequestBody UpdateBucketRequestDto requestDto){
-//        return new Response("bucketId", bucketService.updateBucket(user, requestDto));
-//    }
+    @PutMapping("/")
+    @Operation(summary = "버킷 수정", description = "주어진 id의 버킷을 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "버킷 수정 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "bucketId", schema = @Schema(type = "int", example = "1",description = "수정된 버킷 id"))
+                    }))
+    })
+    public Response updateBucket(@AuthenticationPrincipal User user, @Valid @RequestBody UpdateBucketRequestDto requestDto){
+        return new Response("bucketId", bucketService.updateBucket(user, requestDto));
+    }
 
     @Operation(summary = "버킷 삭제", description = "주어진 id의 버킷을 삭제합니다.")
     @ApiResponses({
@@ -74,29 +84,72 @@ public class BucketController {
         return new Response("message", bucketService.deleteBucket(user, bucket_id));
     }
 
-//    @PatchMapping("/{bucket}")
-//    public Response achieveBucket(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucket_id){
-//        return new Response("message", bucketService.achieveBucket(user, bucket_id));
-//    }
+    @PatchMapping("/{bucket}")
+    @Operation(summary = "버킷 달성일 등록", description = "주어진 id의 버킷 달성일을 금일로 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "버킷 달성일 등록 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "message", schema = @Schema(defaultValue = "버킷 달성일이 등록되었습니다.", description = "달성일 등록 성공 메세지"))
+                    }))
+    })
+    public Response achieveBucket(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucket_id){
+        return new Response("message", bucketService.achieveBucket(user, bucket_id));
+    }
 
-//    @GetMapping("/search")
-//    public Response searchBucket(@RequestParam String word, Pageable pageable){
-//        return new Response("bucketSearchList", bucketService.searchBuckets(word, pageable));
-//    }
+    @GetMapping("/search")
+    @Operation(summary = "버킷 검색", description = "제목에 키워드를 포함하는 버킷를 검색합니다")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "버킷 검색 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "bucketSearchList", schema = @Schema(implementation = BucketSearchResponseDto.class))
+                    }))
+    })
+    public Response searchBucket(@RequestParam String word, Pageable pageable){
+        return new Response("bucketSearchList", bucketService.searchBuckets(word, pageable));
+    }
 
-//    @PostMapping("/image/{bucket}")
-//    public Response bucketImage(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucketId, @RequestParam("bucketImage") MultipartFile bucketImage){
-//        return new Response("bucketImageUrl", bucketService.bucketImage(user, bucketId, bucketImage));
-//    }
+    @PostMapping("/image/{bucket}")
+    @Operation(summary = "버킷 이미지 등록", description = "버킷 이미지를 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "이미지 저장 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "bucketImageUrl",
+                                    schema = @Schema(type = "string",
+                                            example = "(호스트 서버 주소)/image/reviewImage/(이미지명).png",
+                                            description = "이미지 URL"))
+                    }))
+    })
+    public Response bucketImage(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucketId, @RequestParam("bucketImage") MultipartFile bucketImage){
+        return new Response("bucketImageUrl", bucketService.bucketImage(user, bucketId, bucketImage));
+    }
 
-//    @GetMapping("/reaction/{bucket}")
-//    public Response getBucketReaction(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucketId){
-//        return new Response("bucketReaction", bucketService.getBucketReaction(user, bucketId));
-//    }
+    @GetMapping("/reaction/{bucket}")
+    @Operation(summary = "버킷 리액션 조회", description = "버킷에 달린 리액션을 반환합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리액션 조회 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "bucketReaction", schema = @Schema(implementation = GetBucketReactionResponseDto.class)),
+                    }))
+    })
+    public Response getBucketReaction(@AuthenticationPrincipal User user, @PathVariable("bucket") Long bucketId){
+        return new Response("bucketReaction", bucketService.getBucketReaction(user, bucketId));
+    }
 
-//    @PostMapping("/reaction/")
-//    public Response postBucketReaction(@AuthenticationPrincipal User user, @Valid @RequestBody PostBucketReactionRequestDto requestDto){
-//        return new Response("userReaction", bucketService.postBucketReaction(user, requestDto));
-//    }
+    @PostMapping("/reaction/")
+    @Operation(summary = "후기 리액션 작성", description = "후기에 리액션을 답니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리액션 작성 성공",
+                    content = @Content(schemaProperties = {
+                            @SchemaProperty(name = "result", schema = @Schema(defaultValue = "ok", description = "요청 성공")),
+                            @SchemaProperty(name = "userReaction", schema = @Schema(type = "string", example = "멋져요",description = "생성된 리액션 내용"))
+                    }))
+    })
+    public Response postBucketReaction(@AuthenticationPrincipal User user, @Valid @RequestBody PostBucketReactionRequestDto requestDto){
+        return new Response("userReaction", bucketService.postBucketReaction(user, requestDto));
+    }
 
 }
