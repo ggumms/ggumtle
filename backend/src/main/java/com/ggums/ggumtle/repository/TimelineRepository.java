07e950b4a067619a3da2b3c2cc9fab67bd54e166
서준호 <twg0995@naver.com>
2024-02-02 22:03:49 +0900

    [BE] Feat #65: 리뷰 생성시 타임라인 생성
    - 리뷰 생성시 타임라인 생성
    - 리뷰 삭제시 타임라인 삭제
    
    Change-Id: Ia3ce9e8e3c93bfea2b1ab53011f1432ba55dd849

--- backend/src/main/java/com/ggums/ggumtle/repository/TimelineRepository.java
+++ backend/src/main/java/com/ggums/ggumtle/repository/TimelineRepository.java
@@ -1,6 +1,7 @@
 package com.ggums.ggumtle.repository;
 
 import com.ggums.ggumtle.entity.Bucket;
+import com.ggums.ggumtle.entity.Review;
 import com.ggums.ggumtle.entity.Timeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface TimelineRepository extends JpaRepository<Timeline, Long> {
 
     void deleteByBucket(Bucket bucket);
+    void deleteByReview(Review review);
 }
