cf48cbfe6a28a24ea21bad7bb0c937d04a3bcc9b
이우성 <sitpo96@naver.com>
2024-02-06 10:34:07 +0900

    [FE] Fix #76: TextEditor 프리뷰 컴포넌트 제거
    - 기능 개발 전까지 제거해 놓을 예정
    
    Change-Id: I8f124ffb83479fe692bb82a66d3566bd06fae5fb

--- frontend/src/pages/Review/AddReview/TextEditor/index.tsx
+++ frontend/src/pages/Review/AddReview/TextEditor/index.tsx
@@ -1,5 +1,5 @@
 import ReactQuillEditor from './ReactQuillEditor'
-import ReactQuillMirror from './ReactQuillMirror'
+// import ReactQuillMirror from './ReactQuillMirror'
 
 interface ITextEditorProps {
 	value: string
@@ -10,8 +10,8 @@ const TextEditor = ({ value, setValue }: ITextEditorProps) => {
 	return (
 		<>
 			<ReactQuillEditor value={value} setValue={setValue} />
-			{/* //Todo: 나중에 github처럼 html-parser써서 미리보기 화면 전환 가능하도록 제공 예정*/}
-			<ReactQuillMirror value={value} />
+			{/* //Todo: 나중에 github처럼 html-parser써서 미리보기 화면 전환 가능하도록 제공 예정 */}
+			{/* <ReactQuillMirror value={value} /> */}
 		</>
 	)
 }
