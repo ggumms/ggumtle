1d10d60a8fe5f8f5a4cc1585b462b34b72dc7e5a
이원주 <dldnjswn4265@gmail.com>
2024-02-06 23:47:05 +0900

    [FE] Feat #72: 전체탭용 bottomsheet 제작 BucketBottomSheet
    Change-Id: Ifef81c4a1c8f2233c40b957e39de9aaf6589d2a4

---
+++ frontend/src/pages/Radar/components/bottomSheet/BucketBottomSheet.tsx
@@ -0,0 +1,64 @@
+import { MAX_BOTTOM_SHEET_HEIGHT } from '../../../../hooks/usePreviewBottomSheet'
+import Header from '../../../../components/Header'
+import { IMenu, IMenuFunc } from '../../../../interfaces'
+import { icons } from '../../../../constants/header-icons'
+import UserPage from '../../../UserPage'
+import BucketPreview from './BucketPreview'
+import RadarCategoryItems from '../RadarCategory'
+
+interface BucketBottomSheetProp {
+	bucketId: number | null
+	togglePreview: () => void
+	isMaxup: boolean
+	sheet: React.RefObject<HTMLDivElement>
+	content: React.RefObject<HTMLDivElement>
+}
+
+const BucketBottomSheet = (props: BucketBottomSheetProp) => {
+	const { bucketId, togglePreview, isMaxup, sheet, content } = props
+	const menu: IMenu = {
+		left: icons.BACK,
+		center: 'juno의 꿈:틀', // @TODO: 사용자 닉네임 넣기
+		right: icons.HAMBERGER,
+	}
+
+	const func: IMenuFunc = {
+		left_func: () => {
+			togglePreview()
+		},
+		right_func: undefined,
+	}
+	return (
+		<div
+			ref={sheet}
+			style={{ height: `${MAX_BOTTOM_SHEET_HEIGHT}px` }}
+			className={`flex flex-col fixed top-[calc(100%-50px)] left-0 right-0 z-5 rounded-t-2xl shadow-2xl
+		bg-white transition-transform duration-500 ease-out overflow-scroll pb-20`}
+		>
+			{isMaxup ? (
+				<Header menu={menu} func={func} />
+			) : (
+				<div onClick={togglePreview} className="h-10 rounded-t-lg relative pt-5 pb-2">
+					<div className="w-14 h-[5px] rounded-full bg-unActive m-auto" />
+				</div>
+			)}
+			{isMaxup ? (
+				<div ref={content}>
+					<UserPage isForRadar={true} />
+				</div>
+			) : bucketId ? (
+				<div className="px-5 py-2">
+					<BucketPreview bucketId={bucketId} />
+				</div>
+			) : (
+				<div className="px-5 py-3">
+					카테고리 선택
+					<RadarCategoryItems />
+					버튼
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default BucketBottomSheet
