13b7cd0837e3ceded3ee64a615bf9cb8cb47cf43
이원주 <dldnjswn4265@gmail.com>
2024-02-06 23:44:06 +0900

    [FE] Feat #72: BucketItem 컴포넌트
    Change-Id: Id44e5f6ec7872a49f277555b60e1939eaa938511

--- frontend/src/pages/Radar/components/radar/BucketItem.tsx
+++ frontend/src/pages/Radar/components/radar/BucketItem.tsx
@@ -1,13 +1,22 @@
 // @TODO: 데이터 형식 어떻게 받아올지는 추후 논의
 
 import { IRadarBucket } from '../AllTab'
-import { IUserElementStyle, RadarElemType } from './UserItem'
+import { IRadarItemDirection, RadarElemType } from './UserItem'
 
-const UserElementStyle: IUserElementStyle = {
+export interface IBucketStyle {
+	size: string
+}
+
+export interface IBucketItemStyle {
+	first: { style: IBucketStyle; direction: IRadarItemDirection }
+	second: { style: IBucketStyle; direction: IRadarItemDirection }
+	third: { style: IBucketStyle; direction: IRadarItemDirection }
+}
+
+const BucketItemStyle: IBucketItemStyle = {
 	first: {
 		style: {
 			size: 'w-14 h-14',
-			text: 'text-base',
 		},
 		direction: {
 			left: 45,
@@ -17,7 +26,6 @@ const UserElementStyle: IUserElementStyle = {
 	second: {
 		style: {
 			size: 'w-12 h-12',
-			text: 'text-sm',
 		},
 		direction: {
 			left: 45,
@@ -27,26 +35,12 @@ const UserElementStyle: IUserElementStyle = {
 	third: {
 		style: {
 			size: 'w-10 h-10',
-			text: 'text-xs',
 		},
 		direction: {
 			left: 46,
 			bottom: 42,
 		},
 	},
-}
-
-// UserItem 컴포넌트랑 같이 사용할 경우 constants 폴더에 넣어서 사용하기
-const userSize: Record<number, string> = {
-	1: 'w-12 h-12',
-	2: 'w-10 h-10',
-	3: 'w-8 h-8',
-}
-
-const userFontSize: Record<number, string> = {
-	1: 'text-sm',
-	2: 'text-xs',
-	3: 'text-[10px]',
 }
 interface BucketItemProps {
 	bucket: IRadarBucket
@@ -56,16 +50,19 @@ interface BucketItemProps {
 const BucketItem = ({ bucket, type, handleOpenPreview }: BucketItemProps) => {
 	const itemStyle: { position: 'absolute'; left: string; bottom: string } = {
 		position: 'absolute',
-		left: `${UserElementStyle[type].direction.left + bucket.pos.x}%`,
-		bottom: `${UserElementStyle[type].direction.bottom + bucket.pos.y}%`,
+		left: `${BucketItemStyle[type].direction.left + bucket.pos.x}%`,
+		bottom: `${BucketItemStyle[type].direction.bottom + bucket.pos.y}%`,
 	}
-	const name = 'wan'
 	return (
-		<div className="inline-flex flex-col items-center animate-floating" style={itemStyle}>
-			{/* @TODO: api 통신할 때에는 img 태그로 변경 후 실제 데이터 삽입하기
-      <img src="" alt="" /> */}
-			<div className={`bg-green rounded-2xl ${userSize[1]}`}></div>
-			<span className={`${userFontSize[1]} font-semibold`}>{name}</span>
+		<div
+			onClick={() => handleOpenPreview(bucket.bucketId)}
+			className="inline-flex flex-col items-center animate-floating"
+			style={itemStyle}
+		>
+			<div className={`${BucketItemStyle[type].style.size} rounded-2xl overflow-hidden`}>
+				{/* <img src={bucket.bucketPicture} alt="" className="w-full h-full object-cover" /> */}
+				<img src="/public/dummy.PNG" alt="" className="w-full h-full object-cover" />
+			</div>
 		</div>
 	)
 }
