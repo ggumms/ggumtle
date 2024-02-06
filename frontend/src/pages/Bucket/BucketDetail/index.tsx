ac301189d35ff1128487bedfb0180e7432909a11
이우성 <sitpo96@naver.com>
2024-02-06 13:20:44 +0900

    [FE] Feat #133: 버킷 상세 페이지 레이아웃 적용
    Change-Id: I17b16561c0594bb7674f1067272c6ace952e4ced

---
+++ frontend/src/pages/Bucket/BucketDetail/index.tsx
@@ -0,0 +1,43 @@
+import UserProfile from '../../../components/UserProfile'
+import WithHeaderLayout from '../../../components/layout/WithHeaderLayout'
+import { icons } from '../../../constants/header-icons'
+import { useRouter } from '../../../hooks/useRouter'
+import { IMenu, IMenuFunc, UserInfoType } from '../../../interfaces'
+
+const userInfo: UserInfoType = {
+	userId: 1,
+	userProfileImage: 'url',
+	userNickname: 'junho',
+	category: ['인간관계', '여행', '직장'],
+	bucketId: 2,
+	bucketTitle: '구독자 100만명 달성하기',
+	dayCount: 14,
+	color: 'mint',
+	isAchieved: true,
+	owner: true,
+	isFollowing: null,
+}
+
+const BucketDetail = () => {
+	const { goBack } = useRouter()
+
+	const handleLeftFunc = () => {
+		goBack()
+	}
+	const headerMenu: IMenu = {
+		left: icons.BACK,
+		center: `${userInfo.userNickname}의 꿈:틀`,
+		right: undefined,
+	}
+	const headerFunc: IMenuFunc = { left_func: handleLeftFunc, right_func: undefined }
+
+	return (
+		<>
+			<WithHeaderLayout headerMenu={headerMenu} headerFunc={headerFunc}>
+				<UserProfile type="detail" userInfo={userInfo} />
+			</WithHeaderLayout>
+		</>
+	)
+}
+
+export default BucketDetail
