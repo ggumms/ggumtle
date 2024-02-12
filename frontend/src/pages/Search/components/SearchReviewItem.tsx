25c2ed41903a10a2053fd62b35e0424a47f02d61
이원주 <dldnjswn4265@gmail.com>
2024-02-13 04:37:34 +0900

    [FE] Feat #129: 리뷰 검색 결과 클릭시 상세 페이지로 이동
    Change-Id: I081e30899724573af5ca74a8e6948ef5bf11bd0a

--- frontend/src/pages/Search/components/SearchReviewItem.tsx
+++ frontend/src/pages/Search/components/SearchReviewItem.tsx
@@ -4,19 +4,20 @@ import FeedShare from '../../UserPage/utils/FeedShare'
 import TotalReaction from '../../UserPage/components/FeedSection/TotalReaction'
 import TotalComment from '../../UserPage/components/FeedSection/TotalComment'
 import { IReviewSearch } from '../../../store/searchReviewStore'
+import { Link } from 'react-router-dom'
 
-const SearchReviewItem = ({review}: {review: IReviewSearch}) => {
-	const { bucketColor, bucketTitle, reviewTitle, reviewReactionCount, reviewCommentCount} = review
+const SearchReviewItem = ({ review }: { review: IReviewSearch }) => {
+	const { bucketColor, bucketTitle, reviewTitle, reviewReactionCount, reviewCommentCount } = review
 
 	return (
-		<div className="flex flex-col gap-1 px-5 py-4 bg-white">
+		<Link to={`/review/${review.reviewId}`} className="flex flex-col gap-1 px-5 py-4 bg-white">
 			<fieldset className="flex gap-1">
 				<p className={`${textColorClass[bucketColor]} text-xs font-light`}>{bucketTitle}</p>
 				<IoCheckbox size="1rem" className={`rounded-xl ${textColorClass[bucketColor]}`} />
 			</fieldset>
 			<p className="text-point1 text-md font-semibold">{reviewTitle}</p>
 			{/* <p className="line-clamp-2 text-xs text-point1 leading-2 font-light">{content}</p> */}
-			
+
 			<fieldset className="flex justify-between items-center py-1">
 				<div className="flex gap-2">
 					<TotalReaction count={reviewReactionCount} />
@@ -24,7 +25,7 @@ const SearchReviewItem = ({review}: {review: IReviewSearch}) => {
 				</div>
 				<FeedShare />
 			</fieldset>
-		</div>
+		</Link>
 	)
 }
 
