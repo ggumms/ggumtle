76276334e0d1a70bea83ec2a2fcbcdd9bc78fa7e
이원주 <dldnjswn4265@gmail.com>
2024-02-12 16:51:58 +0900

    [FE] Hotfix #74: merge 후 build test 에러 수정
    Change-Id: Ifc5db09a15f8371c2e8344d36a1152161c873af6

--- frontend/src/pages/Search/UserSearch.tsx
+++ frontend/src/pages/Search/UserSearch.tsx
@@ -1,20 +1,6 @@
-import { UserInfoType } from '../../interfaces'
 import SearchUserItem from './components/SearchUserItem'
 
 const UserSearch = () => {
-	const userInfo: UserInfoType = {
-		userId: 1,
-		userProfileImage: 'url',
-		userNickname: 'junho',
-		category: ['인간관계', '여행', '직장'],
-		bucketId: 2,
-		bucketTitle: '구독자 100만명 달성하기',
-		dayCount: 14,
-		color: 'mint',
-		isAchieved: true,
-		owner: true,
-		isFollowing: null,
-	}
 	return (
 		<div className="h-screen px-4">
 			<SearchUserItem />
