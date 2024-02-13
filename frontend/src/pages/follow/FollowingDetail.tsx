354bc9991c898319c2d46ac7ad4f64d6fb5be5e4
전지환 <jeonji0109@naver.com>
2024-02-13 17:16:47 +0900

    [FE] Feat #131: 팔로잉 상세 페이지 UI
    Change-Id: Ife0bd608e9a1bd7ce6aa2e34d7cfc13c7cc328c2

--- frontend/src/pages/follow/FollowingDetail.tsx
+++ frontend/src/pages/follow/FollowingDetail.tsx
@@ -1,5 +1,26 @@
-const FollowingDetail = () => {
-	return <div>Following</div>
-}
+import SearchUserItem from "../Search/components/SearchUserItem";
 
+const FollowingDetail = () => {
+	const userInfo: UserInfoType = {
+		userId: 1,
+		userProfileImage: 'url',
+		userNickname: 'junho',
+		category: ['인간관계', '여행', '직장'],
+		bucketId: 2,
+		bucketTitle: '구독자 100만명 달성하기',
+		dayCount: 14,
+		color: 'mint',
+		isAchieved: true,
+		owner: true,
+		isFollowing: true,
+	}
+	  return (
+		<div className="px-4">
+		  <SearchUserItem user={userInfo} />
+		  <SearchUserItem user={userInfo} />
+		  <SearchUserItem user={userInfo} />
+		  <SearchUserItem user={userInfo} />
+		</div>
+	  );
+	};
 export default FollowingDetail
