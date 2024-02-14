c0e2654e176e4e33ba89a8d883a0f4b588beb90b
이원주 <dldnjswn4265@gmail.com>
2024-02-15 04:22:31 +0900

    [FE] Feat #183: PWA 세팅 html url 주소 수정
    Change-Id: Ie70966bd7ad7e1a083f20b28d3d77d7688b5e341

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useEffect } from 'react'
+import * as Polyfill from 'event-source-polyfill'
 
 function App() {
 	const theme = createTheme({
@@ -19,6 +21,34 @@ function App() {
 				main: '#454645', // 원하는 primary 색상 코드로 변경
 			},
 		},
+	})
+
+
+useEffect(() => {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	let eventSource:any
+	const fetchSse = async() => {
+		try {
+
+			const alarmConnectEndpoint = `${import.meta.env.VITE_BASE_URL}/api/alarm/subscribe`
+			eventSource = new Polyfill.EventSourcePolyfill(alarmConnectEndpoint, {
+				headers: {Authorization: `Bearer ${import.meta.env.VITE_USER1_TOKEN}`,}
+			})
+			
+			eventSource.addEventListener('notify', (event) => {
+				console.log("new notify", event.data)
+			})
+			
+			eventSource.onerror = async (event) => {
+				if (!event.error.message.includes("No activity"))
+					eventSource.close();
+			};
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	fetchSse()
+	return () => eventSource.close();
 	})
 
 	return (
