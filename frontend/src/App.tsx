61a189dce3f039670396e2fc130600652f2f989e
이원주 <dldnjswn4265@gmail.com>
2024-02-16 02:13:06 +0900

    [FE] Feat #183: SSE 통신
    Change-Id: Idcee278b5a1ab177a3a49a2d232acc231f399f7a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
@@ -23,33 +24,69 @@ function App() {
 		},
 	})
 
-
+	// function notifyMe() {
+	// 	if (!("Notification" in window)) {
+	// 		// Check if the browser supports notifications
+	// 		alert("This browser does not support desktop notification");
+	// 	} else if (Notification.permission === "granted") {
+	// 			// Check whether notification permissions have already been granted;
+	// 			// if so, use service worker to create a notification
+	// 			console.log("granded===")
+	// 			navigator.serviceWorker.ready.then(function(registration) {
+	// 					registration.showNotification("Hi there!", {
+	// 							body: "You have a new notification!",
+	// 							icon: '/public/icons/icon-200.png'
+	// 					});
+	// 			});
+	// 	} else if (Notification.permission !== "denied") {
+	// 			// We need to ask the user for permission
+	// 			Notification.requestPermission().then(function(permission) {
+	// 					// If the user accepts, let's create a notification using service worker
+	// 					if (permission === "granted") {
+	// 							navigator.serviceWorker.ready.then(function(registration) {
+	// 									registration.showNotification("Hi there!", {
+	// 											body: "You have a new notification!",
+	// 											icon: '/public/icons/icon-200.png'
+	// 									});
+	// 							});
+	// 					}
+	// 			});
+	// 	}
+	// }
 useEffect(() => {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let eventSource:any
 	const fetchSse = async() => {
 		try {
 
-			const alarmConnectEndpoint = `${import.meta.env.VITE_BASE_URL}/api/alarm/subscribe`
+			const alarmConnectEndpoint = `${import.meta.env.VITE_BASE_URL}alarm/subscribe`
 			eventSource = new EventSourcePolyfill(alarmConnectEndpoint, {
 				headers: {Authorization: `Bearer ${import.meta.env.VITE_USER1_TOKEN}`,}
 			})
+			eventSource.onopen = (event: any) => {
+				console.log('SSE 연결이 열렸습니다.', event);
+			};
 			
-			eventSource.addEventListener('notify', (event: any) => {
-				console.log("new notify", event.data)
+			eventSource.addEventListener('serverEvent', async(event: any) => {
+				console.log(event.data, "알림이 왔어요 serverEvent")
+				const message = event.data;
+				console.log("=======새로운 알림 도착:", JSON.parse(message), '파싱');
+				// notifyMe()
 			})
-			
 			eventSource.onerror = async (event: any) => {
 				if (!event.error.message.includes("No activity"))
+				console.log("close")
 					eventSource.close();
 			};
+
+			
 		} catch (error) {
 			console.log(error)
 		}
 	}
 	fetchSse()
 	return () => eventSource.close();
-	})
+})
 
 	return (
 		<ThemeProvider theme={theme}>
--- frontend/sw.js
+++ frontend/sw.js
@@ -12,3 +12,17 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
 	console.log('[Service Worker] fetched resource ' + e.request.url)
 })
+
+// push alarm
+self.addEventListener('push', function (event) {
+	console.log("이건 되나")
+	const data = event.data.json();
+	const options = {
+			body: data.body,
+			icon: '/public/icons/icon-200.png', // 알림 아이콘 설정
+			badge: '/public/icons/icon-200.png', // 배지 아이콘 설정
+	};
+	event.waitUntil(
+			self.registration.showNotification(data.title, options)
+	);
+});
