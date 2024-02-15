42314ead470d2f47d7f76e5ce6a05a16dcf8c896
이우성 <sitpo96@naver.com>
2024-02-15 13:28:05 +0900

    [FE] Feat : Vite PWA 플러그인 설치
    Change-Id: Ibc10faea9fac64e93c9589838cf6e201e433ee31

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+		}),
+	],
+	build: {
+		manifest: true,
+	},
 })
