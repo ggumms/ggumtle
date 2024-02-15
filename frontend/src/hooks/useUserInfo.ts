de268d3e726fb92a8e918a94fa1b6eb0cb0ef070
이우성 <sitpo96@naver.com>
2024-02-16 01:29:28 +0900

    [FE] Fix : 댓글에서 주인장 프로필 이미지 반영하도록 수정
    Change-Id: If3637ee1c3fd9bf3c0803bedaf2c2494cd6da555

--- frontend/src/hooks/useUserInfo.ts
+++ frontend/src/hooks/useUserInfo.ts
@@ -1,17 +1,17 @@
-import { useQuery } from "@tanstack/react-query"
-import { getUserInfo } from "../pages/Radar/api"
-import { IUserInfo } from "../pages/Radar/types/bottomSheet"
+import { useQuery } from '@tanstack/react-query'
+import { getUserInfo } from '../pages/Radar/api'
+import { IUserInfo } from '../pages/Radar/types/bottomSheet'
 
 export const queryKeys = {
 	userinfo: 'userinfo',
 }
 
-export const useUserInfoQuery = (userId: number) => {
+export const useUserInfoQuery = (userId: number | undefined) => {
 	const { isLoading, data: userInfo } = useQuery<IUserInfo>({
 		queryKey: ['previewUser', userId],
 		queryFn: getUserInfo,
 		enabled: !!userId,
 	})
 
-  return { isLoading, userInfo}
+	return { isLoading, userInfo }
 }
